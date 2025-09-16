0e5805abcee36d3665df7734056122432f36a46c
patidarmk <34210081+patidarmk@users.noreply.github.com>
2025-09-16 20:29:21 +0530

    Add src/pages/Index.tsx

---
+++ src/pages/Index.tsx
@@ -0,0 +1,11 @@
+import { ChatLayout } from "@/components/chat/ChatLayout";
+
+const Index = () => {
+  return (
+    <div className="h-screen bg-background text-foreground">
+      <ChatLayout />
+    </div>
+  );
+};
+
+export default Index;
